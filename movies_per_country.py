8e6fe81461284840c57acf287098a846e61f24c8
GH <81537944+herguad@users.noreply.github.com>
2024-03-29 13:45:18 +0000

    calculate total releases and graph

--- movies_per_country.py
+++ movies_per_country.py
@@ -1,10 +1,9 @@
-
 # Have more nationalities been included on netflix in time?
 # Importing pandas and matplotlib
 import pandas as pd
 import matplotlib.pyplot as plt
 #import numpy as np
-#import seaborn as sns
+import seaborn as sns
 
 netflix_df=pd.read_csv('netflix_data.csv')
 # Read in the Netflix CSV as a DataFrame
@@ -15,15 +14,22 @@ netflix_subset = netflix_df[netflix_df["type"] == "Movie"]
 
 # Select only the columns of interest
 netflix_movies_nat = netflix_subset[["country", "release_year"]]
-netflix_movies_year=  netflix_subset[["release_year"]]
-#Group movies by country and count them
+
+#Group movies per release_year and nationality
 movies_per_c = netflix_movies_nat.groupby(["country"]).value_counts().reset_index(name="count")
+
+#print(movies_per_c)
 
 #List South American countries and select movies released by year and country
 sa_countries=["Argentina", "Bolivia","Brazil","Chile","Colombia","Ecuador","Guyana","Paraguay","Peru","Suriname","Uruguay","Venezuela"]
 sa_movies_per_y = movies_per_c[movies_per_c["country"].isin(sa_countries)]
 
-#Calculate max number of movies released each year and sort by county and country
-
-print(sa_movies_per_y)
-#Graph release_year agnst nationality with scatterplot: use color for nationality and size for n of movies.
+#Calculate total releases per country and sort by country
+total_releases_per_c= sa_movies_per_y.groupby(["release_year","country"])[["release_year","count"]].sum()
+total_r_p_country= pd.DataFrame(total_releases_per_c)
+#Calculate max number of movies released each year and sort by count and country
+#max_releases_per_y=sa_movies_per_y.groupby("release_year")[["country","count"]].max()
+time=list(sa_movies_per_y["release_year"])
+#Graph total_releases_per_c from 1985 to 2020
+sns.scatterplot(data=total_r_p_country, x="release_year", y="count", hue="country", palette="deep",markers= time)
+plt.show()
