fdb356cbac55477f15b69397c0ef32414f368935
GH <81537944+herguad@users.noreply.github.com>
2024-04-07 12:24:05 -0300

    Update movies_distrib_nationalities.py

--- movies_distrib_nationalities.py
+++ movies_distrib_nationalities.py
@@ -6,15 +6,17 @@ import matplotlib.pyplot as plt
 #import numpy as np
 import seaborn as sns
 
-#Distribution per continent
-#List countries represented in Netflix library.
-netflix_df=pd.read_csv('netflix_data.csv')
 # Read in the Netflix CSV as a DataFrame
 netflix_df = pd.read_csv("netflix_data.csv")
+# Subset the DataFrame for type "Movie"
+netflix_subset = netflix_df[netflix_df["type"] == "Movie"]
 
-# Subset the DataFrame for type "country". Filter NaNs and eliminate duplicates.
-netflix_subset_c_noNaN = netflix_df["country"].dropna()
-
+#Distribution per continent
+# Subset the DataFrame for type "country". Filter NaNs.
+netflix_subset_c_noNaN = netflix_subset["country"].dropna()
+netflix_countries_filtered=pd.DataFrame(netflix_subset_c_noNaN)
+netflix_nat_year=netflix_countries_filtered.merge(netflix_subset, on="country",how="left")
+print(netflix_nat_year)
 #Distribution per year and continent
 
 #Visualizing the distributions and finding the most and least represented nationalities in Netflix library.
