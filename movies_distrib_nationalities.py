b7bcd8b5fa88b57fe9d0b8d8490fbb63af3ed9f2
GH <81537944+herguad@users.noreply.github.com>
2024-04-07 15:37:23 -0300

    Update movies_distrib_nationalities.py

--- movies_distrib_nationalities.py
+++ movies_distrib_nationalities.py
@@ -19,8 +19,8 @@ netflix_nat_year=netflix_countries_filtered.merge(netflix_subset, on="country",h
 movies_col_select=netflix_nat_year[['country','date_added','genre']]
 movies_col_select['date_added']=pd.to_datetime(movies_col_select['date_added'])
 movies_col_select['date_added']=movies_col_select['date_added'].dt.year  #Ignore warning but read: https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
-movies_grouped_sum=movies_col_select.groupby("country").value_counts()
-print(movies_grouped_sum)
+movies_grouped_sum=movies_col_select.groupby(["country","date_added"]).value_counts(ascending=True).reset_index(name='movies_per_countryear')
+#print(movies_grouped_sum)
 #Distribution per year and continent
 
 #Visualizing the distributions and finding the most and least represented nationalities in Netflix library.
