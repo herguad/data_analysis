f21eed57fe65c9e999f68592ade351945d7f84b1
GH <81537944+herguad@users.noreply.github.com>
2024-04-12 13:25:06 -0300

    start visualizing.py

--- movies_distrib_nationalities.py
+++ movies_distrib_nationalities.py
@@ -35,7 +35,7 @@ movies_col_select.loc[:,('date_added')]=pd.to_datetime(movies_col_select.loc[:,(
 movies_col_select['year_added']=movies_col_select['date_added'].values  #Ignore warning but read: https://pandas.pydata.org/pandas-docs/stable/user_guide/indexing.html#returning-a-view-versus-a-copy
 movies_col_select['year_added']=movies_col_select['year_added'].dt.year
 
-#Adddifferent dfs selecting release_year and country, release_year and genre and genre and country separately
+#Add different dfs selecting release_year and country, release_year and genre and genre and country separately
 movies_country_genre_year=movies_col_select.loc[:,('country','genre','year_added')]
 movies_country_year=movies_col_select.loc[:,('country','year_added')]
 movies_genre_year=movies_col_select.loc[:,('genre','year_added')]
@@ -49,9 +49,12 @@ movies_g_y_sum=movies_genre_year.groupby(["genre","year_added"]).value_counts(as
 
 #Group movies by country andgenre, and count genres movies added per country. 
 movies_g_c_sum=movies_country_genre.groupby(["country","genre"]).value_counts(ascending=True).reset_index(name='movies_per_countrygenre')
-print(movies_g_c_sum.head(10))
 
-#sns.histplot(data=movies_grouped_sum, x="date_added", y="movies_per_countryear")
+#Visualize distribution of movies released per year (tbc)
+sns.scatterplot(data=movies_c_y_sum, x="year_added", y="movies_per_countryear")
+plt.xlim(2005,2024)
+plt.ylim(0,1100)
+plt.show()
 
 #Distribution per year and continent
 
@@ -59,3 +62,7 @@ print(movies_g_c_sum.head(10))
 
 #Make dictionary using awoc lists of contientns and countries and country column from netflix df
 #continents_countries={}
+
+
+
+#Visualizing the distributions and finding the most and least represented nationalities in Netflix library.
