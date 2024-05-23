74282f5c731a0b736195946e633176407fadc681
GH <81537944+herguad@users.noreply.github.com>
2024-05-23 11:50:25 -0300

    Update movies_distrib_genre.py
    remove uncategorized movies rows and check

--- code/movies_distrib_genre.py
+++ code/movies_distrib_genre.py
@@ -32,7 +32,10 @@ print(movies_g_c_sum)
 #Count uncategorized movies and remove/display on their own (by country and by year). Add to NaN Movies file.
 uncategorized_s=movies_g_y_sum[movies_g_y_sum['genre']=='Uncategorized']['movies_per_genreyear'].sum()
 uncategorized=movies_g_y_sum[movies_g_y_sum['genre']=='Uncategorized']
-print(uncategorized)
+#print(uncategorized)
+movies_genre_year=movies_g_y_sum.drop(index=[107,108,109,110,111,112,113])
+print(movies_genre_year)
+
 #Find whether there's a correlation between genre and number of movies released each year . --> sns. heatmap
 #Find whether there's a correlation between genre and country . --> sns. heatmap
 #Find whether there's a correlation between genre and continent . --> sns. heatmap
